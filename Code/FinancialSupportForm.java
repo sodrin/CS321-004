908979c988c7e9c7a7726232039aced403b38644
sodrin <greyfecta@yahoo.com>
2023-10-02 14:35:39 -0400

    FinancialSupportForm java file added

---
+++ Code/FinancialSupportForm.java
@@ -0,0 +1,37 @@
+/**
+* This is the financial support form class which contains form information.
+*
+*/
+
+public class FinancialSupportForm{
+  int ID;
+  String FirstName;
+  String LastName;
+  String SSN;
+  String ApplicantEmail;
+  String RecipientFirstName;
+  String RecipientLastName;
+  String RecipientDateOfBirth;
+  String RecipientAlienNumber;
+  String RecipientEmail;
+
+  // constructor
+  public FinancialSupportForm(){
+
+  }
+
+  //validation method
+  public void validate(){
+
+  }
+
+  //saveform method
+  public boolean saveForm(){
+    return false;
+  }
+
+  //getform method
+  public FinancialSupportForm getForm(){
+    return null;
+  }
+}
--- Code/Test.java
+++
@@ -1,3 +0,0 @@
-public class Test{
-  int test1 = 0;
-}

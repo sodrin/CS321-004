2f3844c1bd44636e99655e8bd3a676e93471a5c0
unknown <senan.geraghty@outlook.com>
2023-10-16 14:30:34 -0400

    shared object set up

--- Code/FinancialSupportFormTester.java
+++ Code/FinancialSupportFormTester.java
@@ -3,10 +3,12 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class FinancialSupportFormTester {
-    
+
+    FinancialSupportForm form = new FinancialSupportForm();
+
     @Test
     void test1() {
-        FinancialSupportForm form = new FinancialSupportForm();
-        assertEquals(form.getForm(), null);
+        
+        assertEquals(form, form.getForm(123));
     }
 }

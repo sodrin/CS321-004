049b03a1dd428fb01ee3e1a3a35a3e680bb077f1
caiden20000 <caiden2000offline@gmail.com>
2023-10-18 12:47:47 -0400

    Implement add/get via Queues

--- Code/WorkflowTable.java
+++ Code/WorkflowTable.java
@@ -1,20 +1,32 @@
+import java.util.Queue;
+
 public class WorkflowTable{
-  int[] PendingReview;
-  int[] PendingApproval;
+  Queue<Integer> pendingReviews;
+  Queue<Integer> pendingApprovals;
+
+  FinancialSupportForm BO = new FinancialSupportForm();
 
   FinancialSupportForm getNextPendingReview(){
-    return null;
+    Integer formID = pendingReviews.poll();
+    if (formID == null) return null;
+    // TODO: We need some static way to get forms from form IDs, unless
+    //   we are using one global BusinessObject instance.
+    return BO.getForm(formID);
   }
 
   FinancialSupportForm getNextPendingApproval(){
-    return null;
+    Integer formID = pendingApprovals.poll();
+    if (formID == null) return null;
+    return BO.getForm(formID);
   }
 
   Boolean addPendingReview(FinancialSupportForm form){
-    return false;
+    Boolean result = pendingReviews.offer(form.ID);
+    return result;
   }
 
   Boolean addPendingApproval(FinancialSupportForm form){
-    return false;
+    Boolean result = pendingApprovals.offer(form.ID);
+    return result;
   }
 }

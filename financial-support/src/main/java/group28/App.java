437f4d1f883ecc69f80696f5175b90a056c72b20
unknown <senan.geraghty@outlook.com>
2023-11-01 15:46:01 -0400

    main menu

--- financial-support/src/main/java/group28/App.java
+++ financial-support/src/main/java/group28/App.java
@@ -1,10 +1,14 @@
 package group28;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
 
 
 /**
@@ -14,11 +18,43 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        
+        //Button to launch data entry screen
+        Button entryButton = new Button();
+		entryButton.setText("Data Entry");
+        entryButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+                EntryScreen screen = new EntryScreen();
+                screen.showScreen();
+            }
+        });
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        //Button to launch review screen
+        Button reviewButton = new Button();
+		reviewButton.setText("Review");
+        reviewButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+                ReviewScreen screen = new ReviewScreen();
+                screen.showScreen();
+            }
+        });
+
+        //Button to launch approval screen
+        Button approvalButton = new Button();
+		approvalButton.setText("Approval");
+        approvalButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+                ApprovalScreen screen = new ApprovalScreen();
+                screen.showScreen();
+            }
+        });
+
+
+        StackPane root = new StackPane(new HBox(entryButton, reviewButton, approvalButton));
+        Scene scene = new Scene(root, 450, 250);
         stage.setScene(scene);
         stage.show();
     }

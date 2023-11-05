f8c07e4f572192f6a794037e9509b1a5c63fbe5c
caiden20000 <caiden2000offline@gmail.com>
2023-11-05 09:02:28 -0500

    Track db size with public int

--- financial-support/src/main/java/group28/Database.java
+++ financial-support/src/main/java/group28/Database.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class Database {
     private static HashMap<Integer, FinancialSupportForm> forms = new HashMap<Integer, FinancialSupportForm>();
+    public static int databaseSize = 0;
     
     // Returns null if the form does not exist
     public static FinancialSupportForm getForm(int id) {
@@ -13,6 +14,14 @@ public class Database {
     // Returns true if the form already existed.
     // Returns false if the form did not exist yet. Still sets the form if returns false.
     public static boolean setForm(FinancialSupportForm form) {
-        return forms.put(form.ID, form) != null;
+        boolean newForm = forms.put(form.ID, form) != null;
+        // Keep track of the size of the database
+        // Database doesn't shrink 'cause there isn't a way to delete forms yet
+        if (newForm) databaseSize ++;
+        return newForm;
+    }
+
+    public static Integer[] getKeys() {
+        return forms.keySet().toArray(new Integer[]{});
     }
 }

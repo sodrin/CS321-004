f54843424c1fcdc1277b72bf4cf860ced57599e7
caiden20000 <caiden2000offline@gmail.com>
2023-11-05 08:43:59 -0500

    Add get/set to database, making it usable

--- financial-support/src/main/java/group28/Database.java
+++ financial-support/src/main/java/group28/Database.java
@@ -1,7 +1,18 @@
 package group28;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
-    public static ArrayList<FinancialSupportForm> forms;
+    private static HashMap<Integer, FinancialSupportForm> forms = new HashMap<Integer, FinancialSupportForm>();
+    
+    // Returns null if the form does not exist
+    public static FinancialSupportForm getForm(int id) {
+        return forms.get(id);
+    }
+
+    // Returns true if the form already existed.
+    // Returns false if the form did not exist yet. Still sets the form if returns false.
+    public static boolean setForm(FinancialSupportForm form) {
+        return forms.put(form.ID, form) != null;
+    }
 }

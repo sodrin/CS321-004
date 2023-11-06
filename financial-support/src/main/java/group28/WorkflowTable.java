dd16e7102f7a3a7a3bfc1a62f57ffe1819cef193
caiden20000 <caiden2000offline@gmail.com>
2023-11-06 10:56:32 -0500

    Add singleton

--- financial-support/src/main/java/group28/WorkflowTable.java
+++ financial-support/src/main/java/group28/WorkflowTable.java
@@ -4,32 +4,33 @@ import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class WorkflowTable{
+  // Singleton instance for consistant global access
+  public static WorkflowTable masterTable = new WorkflowTable();
+
   Queue<Integer> pendingReviews = new LinkedBlockingQueue<Integer>();
   Queue<Integer> pendingApprovals = new LinkedBlockingQueue<Integer>();
 
-  FinancialSupportForm BO = new FinancialSupportForm();
-
   // Returns null if the queue is empty
-  FinancialSupportForm getNextPendingReview(){
+  public FinancialSupportForm getNextPendingReview(){
     Integer formID = pendingReviews.poll();
     if (formID == null) return null;
     return FinancialSupportForm.getForm(formID);
   }
 
   // Returns null if the queue is empty
-  FinancialSupportForm getNextPendingApproval(){
+  public FinancialSupportForm getNextPendingApproval(){
     Integer formID = pendingApprovals.poll();
     if (formID == null) return null;
     return FinancialSupportForm.getForm(formID);
   }
 
   // Returns true if the element was added successfully
-  Boolean addPendingReview(FinancialSupportForm form){
+  public Boolean addPendingReview(FinancialSupportForm form){
     return pendingReviews.offer(form.ID);
   }
 
   // Returns true if the element was added successfully
-  Boolean addPendingApproval(FinancialSupportForm form){
+  public Boolean addPendingApproval(FinancialSupportForm form){
     return pendingApprovals.offer(form.ID);
   }
 }

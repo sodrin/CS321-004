a2c0b3aa89bfdc22c35c44b12b060c0c8c0b7575
caiden20000 <caiden2000offline@gmail.com>
2023-11-06 12:27:32 -0500

    Link ApprovalScreen to the controller

--- financial-support/src/main/java/group28/ApprovalScreen.java
+++ financial-support/src/main/java/group28/ApprovalScreen.java
@@ -11,25 +11,52 @@ import javafx.stage.Stage;
  */
 public class ApprovalScreen {
 
-	private int employeeId;
-	public int currentFormId;
+	private int employeeId = -1;
+	public int currentFormId = -1;
+	private ApprovalScreenController controller;
 
 	public boolean authenticate(String username, String password, int employeeId) {
-		// TODO
+		// TODO authentication here
+		boolean authenticated = true;
+
+		if (authenticated) {
+			this.employeeId = employeeId;
+			return true; 
+		}
+
 		return false;
 	}
 	
 	public boolean getNextForm() {
 		// TODO
+		if (controller == null) return false;
+		FinancialSupportForm nextForm = WorkflowTable.masterTable.getNextPendingApproval();
+		if (nextForm != null) {
+			controller.displayForm(nextForm);
+			controller.formMode();
+			return true;
+		}
 		return false;
+	}
+
+	public void approveForm() {
+		this.approveForm(currentFormId);
 	}
 
 	public void approveForm(int formId) {
 		// TODO
+		controller.clearForm();
+		controller.noFormMode();
 	}
 
+	public void sendFormToReview() {
+		this.sendFormToReview(currentFormId);
+	}
+	
 	public void sendFormToReview(int formId) {
 		// TODO
+		controller.clearForm();
+		controller.noFormMode();
 	}
     
 	// Entry code for the ApprovalScreen class
@@ -47,7 +74,8 @@ public class ApprovalScreen {
 			newWindow.show();
 
 			// Initialize the form's state
-			ApprovalScreenController controller = fxmlView.getController();
+			controller = fxmlView.getController();
+			controller.setMasterScreen(this);
 			controller.clearForm();
 			controller.noFormMode();
 		} catch (IOException e) {
